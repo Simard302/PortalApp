bc74147f42b2026a90209d94ce4d052425d9d88f
Simard302 <Simard302@users.noreply.github.com>
2021-01-18 13:41:06 -0500

    Added basic functions for other app into main

---
+++ main.py
@@ -0,0 +1,51 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+from data import UserDatabase
+from screen_format import build_string
+Window.size = (450, 500)
+
+
+class LoginScreen(Screen):
+    pass
+
+
+class RegistrationScreen(Screen):
+    pass
+
+class Main(MDApp):
+    def build(self):
+        self.database = UserDatabase()
+
+        screen = Builder.load_string(build_string)
+
+        self.theme_cls.primary_palette = "Purple"
+        sm = ScreenManager()
+        screens = [Screen(name='Title {}'.format(i)) for i in range(4)]
+        sm.add_widget(LoginScreen(name='login'))                # 0
+        sm.add_widget(RegistrationScreen(name='registration'))  # 1
+
+        self.sm = sm
+        self.screens = screens
+        return screen
+
+    def doRegistration(self, email, password1, password2):
+        if password1 == password2:
+            self.database.CreateUser(email, password1)
+            print(email)
+            print(password1)
+        else :
+            # Give the client an error here
+            print(email)
+            print(password1)
+            print(password2)
+
+
+    def verifyLogin(self, email, password):
+        print(email)
+        print(password)
+
+
+if __name__ == "__main__":
+    Main().run()
